d58d29a9f79312a68a3fc1f2102ccddeff87eb11
unknown <rakibulhasan.seu.cse@gmail.com>
2023-05-02 23:07:27 +0600

    recipe information by chef id done

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+// importing data
+const chefData = require('./data/chef.json');
+const recipeData = require('./data/recipe.json');
+
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.send("Responding");
 })
 
+// chefs info
+app.get("/chef", (req, res) => {
+    res.send(chefData);
+})
+
+// recipe info by chefs id
+app.get("/chef/:id", (req, res) => {
+    const id = req.params.id;
+    const recipeInfo = recipeData.filter((recipe) => recipe.chef_id == id);
+    res.send(recipeInfo);
+})
+
+
+// server run checking
 app.listen(port, () => {
     console.log(`Sever is running on ${port}`);
 })
